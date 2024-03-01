ba7f74666ee7a7bf994c0a94c9b83a08aae2c1bf
Marilia <ramonap2013@gmail.com>
2024-02-29 23:18:24 -0600

    firebase configuration

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,42 @@
 from flask import Flask, render_template, request
+from flask_cors import CORS
+import logging
 import os
+from datetime import datetime, timedelta, timezone
+import pyrebase
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
+CORS(app)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# configuration of firebase
+config = {
+    "apiKey": "apiKey",
+    "authDomain": "projectId.firebaseapp.com",
+    "databaseURL": "https://databaseName.firebaseio.com",
+    "storageBucket": "projectId.appspot.com",
+    "serviceAccount": "path/to/serviceAccountCredentials.json"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+# use realtime database
+db = firebase.database()
+
+#write data
+data = {"name": "John Doe", "email": "johndoe@example.com"}
+db.child("users").push(data)
+
+#read data
+users = db.child("users").get()
+for user in users.each():
+    print(user.val())
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return "hello world"
 
 #If student assigns to the student class
 
