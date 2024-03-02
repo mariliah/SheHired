092b1d9d978d6ff1deb1e023ec4065ab4fd51da5
Marilia <ramonap2013@gmail.com>
2024-03-01 20:50:14 -0600

    models, and services

---
+++ backend/models/users.py
@@ -0,0 +1,10 @@
+class User:
+    def __init__(self, email, password, name, role, bio, work_history, education, demographic):
+        self.email = email
+        self.password = password
+        self.name = name
+        self.role = role
+        self.bio = bio
+        self.work_history = work_history
+        self.education = education
+        self.demographic = demographic
