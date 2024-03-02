fb80dcd14f3abe08753091057073ebab116e6a46
Amelia Dutta <49182604+amelia2802@users.noreply.github.com>
2024-03-02 14:41:56 +0000

    disabled traditional ccs

--- frontend/src/app/signup/signup.js
+++ frontend/src/app/signup/signup.js
@@ -4,19 +4,27 @@ import brandImg from "../../images/joel-muniz-HvZDCuRnSaY-unsplash 1.png";
 
 export default function Signup() {
   return (
-    <section className="signupLayout">
+    <section>
       <img src={Logo} alt="" className="reg-logo" />
       <section className="signupBody">
         <img src={brandImg} alt="brand logo" className="signupLogo" />
 
-        <section className="signup-form">
+        <section className="md:block">
           <h1>CREATE A NEW ACCOUNT</h1>
           ENTER YOUR DETAILS TO REGISTER
-          <form action="" className="signup">
+          <form action="">
             <label htmlFor="name">NAME</label>
             <input id="name" type="text" />
             <label htmlFor="email">EMAIL ADDRESS</label>
-            <input id="email" type="email" />
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autocomplete="email"
+                required
+              />
+            </div>
             <label htmlFor="password">PASSWORD</label>
             <input id="password" type="password" />
           </form>
@@ -27,7 +35,7 @@ export default function Signup() {
             <label htmlFor="utype">EARLY CAREER</label>
             <button>SIGN UP</button>
             ALREADY HAVE AN ACCOUNT?
-            <a href="" className="loginLink">
+            <a href="www.example.com" className="loginLink">
               LOGIN HERE
             </a>
           </section>
