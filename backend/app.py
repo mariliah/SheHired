d589017b156b2deba6d36da5294c927528ffc7d8
Marilia <ramonap2013@gmail.com>
2024-03-03 22:19:04 -0600

    added authorization to each route

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,9 @@ import logging
 import os, json
 import jsonify
 from datetime import datetime, timedelta, timezone
+from functools import wraps
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore, auth
 import uuid
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
@@ -14,7 +15,7 @@ CORS(app)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-from controllers.student_controller import view_internships_controller, apply_for_job_controller as apply_for_student_job_controller
+from controllers.student_controller import view_internships_controller, apply_for_internship_controller as apply_for_student_internship_controller
 from controllers.professional_controller import view_jobs_controller, upload_resume_controller, apply_for_job_controller as apply_for_professional_job_controller
 from controllers.recruiter_controller import *
 
@@ -52,42 +53,64 @@ job_postings = []
 internship_applications = []
 resume_storage = []
 
+def check_auth(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if not 'Authorization' in request.headers:
+            return jsonify({'message': 'No token provided'}), 403
+        id_token = request.headers['Authorization'].split(' ').pop()
+        try:
+            decoded_token = auth.verify_id_token(id_token)
+            request.uid = decoded_token['uid']
+        except:
+            return jsonify({'message': 'Invalid token'}), 403
+        return f(*args, **kwargs)
+    return wrap
+
 @app.route('/')
 def index():
     return "hello world"
 
 # ROUTES FOR RECRUTIERS
 @app.route('/recruiters/create_job', methods=['POST'])
+@check_auth
 def create_job_route():
     return create_job_controller()
 
 @app.route('/recruiters/update_job/<job_id>', methods=['PUT'])
+@check_auth
 def update_job_route(job_id):
     return update_job_controller(job_id)
 
 @app.route('/recruiters/deactivate_job/<job_id>', methods=['PUT'])
+@check_auth
 def deactivate_job_route(job_id):
     return deactivate_job_controller(job_id)
 
 # ROUTES FOR STUDENTS
 @app.route('/students/view_internships', methods=['GET'])
+@check_auth
 def view_internships_route():
     return view_internships_controller()
 
 @app.route('/students/apply_for_job/<job_id>', methods=['POST'])
+@check_auth
 def apply_for_student_job_route(job_id):
     return apply_for_student_internship_controller(job_id)
 
 # ROUTES FOR PROFESSIONALS
 @app.route('/professionals/view_jobs', methods=['GET'])
+@check_auth
 def view_jobs_route():
     return view_jobs_controller()
 
 @app.route('/professionals/upload_resume', methods=['POST'])
+@check_auth
 def upload_resume_route():
     return upload_resume_controller()
 
 @app.route('/professionals/apply_for_job/<job_id>', methods=['POST'])
+@check_auth
 def apply_for_professional_job_route(job_id):
     return apply_for_professional_job_controller(job_id)
 
